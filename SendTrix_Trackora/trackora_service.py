2bea7e668b928e105a8d685cf52d1b7f6210bf2a
Debu1999 <debudebasis1999@gmail.com>
2026-08-29 10:12:27 +0530

    apply and ignore all changes

--- SendTrix_Trackora/trackora_service.py
+++ SendTrix_Trackora/trackora_service.py
@@ -468,3 +468,31 @@ def ignore_change_db(change_id):
  
     conn.commit()
     conn.close()
+
+def apply_all_changes_db(comparison_id):
+ 
+    conn = get_connection()
+    cursor = conn.cursor()
+ 
+    cursor.execute("""
+    UPDATE comparison_changes
+    SET approval_status = 'APPROVED'
+    WHERE comparison_id = %s
+    """, (comparison_id,))
+ 
+    conn.commit()
+    conn.close()
+ 
+def ignore_all_changes_db(comparison_id):
+ 
+    conn = get_connection()
+    cursor = conn.cursor()
+ 
+    cursor.execute("""
+    UPDATE comparison_changes
+    SET approval_status = 'IGNORED'
+    WHERE comparison_id = %s
+    """, (comparison_id,))
+ 
+    conn.commit()
+    conn.close()
