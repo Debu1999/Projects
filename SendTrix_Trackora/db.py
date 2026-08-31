f3812aa2ef98fb8d033e6b50df4dfcf29bd8b15e
Debu1999 <debudebasis1999@gmail.com>
2026-08-31 08:48:35 +0530

    commenting out the local postgress get_connection

--- SendTrix_Trackora/db.py
+++ SendTrix_Trackora/db.py
@@ -8,16 +8,16 @@ from dotenv import load_dotenv
 from flask import session
 
 load_dotenv()
-def get_connection():
+'''def get_connection():
     return psycopg.connect(
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT"),
         dbname=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD")
-    )
-'''def get_connection():
-    return psycopg.connect(os.getenv("DATABASE_URL"))'''
+    )'''
+def get_connection():
+    return psycopg.connect(os.getenv("DATABASE_URL"))
 '''def init_db():
     conn = get_connection()
  
